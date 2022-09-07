ade7645ceb8c1a465768a2084b70199f168c6955
Boutchouangemmanuel <emmanuelgireh@gmail.com>
2022-09-07 04:18:40 -0700

    updated task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,6 @@
--- Create table 'second_table' in db 'hbtn_0c_0'
--- Add descriptions (id INT), (name VARCHAR(256)), (score INT)
--- db name will be passed as arg to mysql cmd
--- Not allowed to use SELECT or SHOW
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-
--- Script should create 4 records
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Emmanuel", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alexis", 8);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bobda", 17);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Manu", 7);
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
